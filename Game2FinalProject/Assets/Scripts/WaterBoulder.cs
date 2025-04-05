bd56bc06c71ab3800650edc6f1af6054fd1c683f
Nova Thomas <novathomas2002@gmail.com>
2025-04-05 18:54:12 -0400

    Water Boulder Implemented

--- Game2FinalProject/Assets/Scripts/WaterBoulder.cs
+++ Game2FinalProject/Assets/Scripts/WaterBoulder.cs
@@ -2,22 +2,47 @@ using NETWORK_ENGINE;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.ParticleSystem;
 
 public class WaterBolder : NetworkComponent
 {
+    private Rigidbody rb; // Reference to the Rigidbody component
+    public float knockbackForce = 10f; // Adjust this value to control knockback strength
+    public Transform lastHitPos;
+
+    public Vector3 Vector3FromString(string s)
+    {
+        string[] args = s.Trim().Trim('(').Trim(')').Split(',');
+        return new Vector3(float.Parse(args[0]), float.Parse(args[1]), float.Parse(args[2]));
+    }
+
     public override void HandleMessage(string flag, string value)
     {
-        throw new System.NotImplementedException();
+        if (flag == "HIT")
+        {
+            if(IsServer)
+            {
+                Vector3 knockbackDirection = Vector3FromString(value);
+                rb.AddForce(knockbackDirection * knockbackForce, ForceMode.Impulse);
+            }
+        }
     }
 
     public override void NetworkedStart()
     {
-        throw new System.NotImplementedException();
+        rb = GetComponent<Rigidbody>();
     }
 
     public override IEnumerator SlowUpdate()
     {
-        throw new System.NotImplementedException();
+        while (IsConnected)
+        {
+            if (IsDirty)
+            {
+                IsDirty = false;
+            }
+            yield return new WaitForSeconds(0.1f);
+        }
     }
 
     // Start is called before the first frame update
@@ -30,5 +55,15 @@ public class WaterBolder : NetworkComponent
     void Update()
     {
         
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "WaterBlast" && IsClient)
+        {
+            lastHitPos = other.transform;
+            Vector3 knockbackDirection = (transform.position - lastHitPos.position).normalized;
+
+            SendCommand("HIT", knockbackDirection.ToString());
+        }
     }
 }
